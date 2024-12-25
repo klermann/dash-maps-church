1426454028921d7f901922f37a1f4c1423c33ea5
clerman <klermann@hotmail.com>
2024-12-25 19:29:27 -0300

    Validação no form Adicionar Igreja
    Validação no form Adicionar Igreja

--- frontend/vendors/jQuery-Smart-Wizard/js/jquery.smartWizard.js
+++ frontend/vendors/jQuery-Smart-Wizard/js/jquery.smartWizard.js
@@ -21,9 +21,9 @@ function SmartWizard(target, options) {
     this.elmStepContainer = $('<div></div>').addClass("stepContainer");
     this.loader = $('<div>Loading</div>').addClass("loader");
     this.buttons = {
-        next : $('<a>'+options.labelNext+'</a>').attr("href","#").addClass("buttonNext"),
-        previous : $('<a>'+options.labelPrevious+'</a>').attr("href","#").addClass("buttonPrevious"),
-        finish  : $('<a>'+options.labelFinish+'</a>').attr("href","#").addClass("buttonFinish")
+        next : $('<a>'+options.labelNext+'</a>').attr("href","#").addClass("buttonNext btn-sm"),
+        previous : $('<a>'+options.labelPrevious+'</a>').attr("href","#").addClass("buttonPrevious btn-sm"),
+        finish  : $('<a id="submitButton" >'+options.labelFinish+'</a>').attr("href","#").addClass("buttonFinish btn-sm")
     };
 
     /*
@@ -52,9 +52,9 @@ function SmartWizard(target, options) {
         $this.elmStepContainer.append(allDivs);
         elmActionBar.append($this.loader);
         $this.target.append($this.elmStepContainer);
-        elmActionBar.append($this.buttons.finish)
+        elmActionBar.append($this.buttons.previous)
                     .append($this.buttons.next)
-                    .append($this.buttons.previous);
+                    .append($this.buttons.finish);
         $this.target.append(elmActionBar);
         this.contentWidth = $this.elmStepContainer.width();
 
@@ -439,7 +439,7 @@ $.fn.smartWizard.defaults = {
     errorSteps:[],    // Array Steps with errors
     labelNext:'Próximo',
     labelPrevious:'Anterior',
-    labelFinish:'Fim',
+    labelFinish:'Enviar',
     noForwardJumping: false,
     onLeaveStep: null, // triggers when leaving a step
     onShowStep: null,  // triggers when showing a step
